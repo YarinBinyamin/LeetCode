c6ed458087f22b03e09675afa382aab73f6c56e6
YarinBinyamin <yarin10binyamin@gmail.com>
2025-03-29 14:38:44 +0300

    Update MainPython.py

--- src/MainPython.py
+++ src/MainPython.py
@@ -1,2 +1,13 @@
 class Solution(object):
-    def generateMatrix(self, n):
+    def climbStairs(self, n: int) -> int:
+        ans_dp = [0] * (n + 1)
+        ans_dp[0] = 1
+        ans_dp[1] =1
+        for i in range(2 , n +1, 1):
+            ans_dp[i] = ans_dp[i-1] + ans_dp[i-2]
+        return ans_dp[n]
+    
+if __name__ == "__main__":
+    sol = Solution()
+    result = sol.climbStairs(10)
+    print("Result:", result)
