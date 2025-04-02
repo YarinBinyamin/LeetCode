c22a1745adb1d9ca2fc9627675ded194065baaed
YarinBinyamin <yarin10binyamin@gmail.com>
2025-04-02 15:19:47 +0300

    Update MainPython.py

--- src/MainPython.py
+++ src/MainPython.py
@@ -7,6 +7,18 @@ class Solution(object):
             ans_dp[i] = ans_dp[i-1] + ans_dp[i-2]
         return ans_dp[n]
     
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        if path.index(size(path)-1) == '/':
+           path = path[:-1]
+        start = 0
+        for i in range(1,size(path) -1, +1):
+            if path.index(i).isalpha():
+                continue
+            if path.index(i) == '.':
+                start = 1
+                    
+    
 if __name__ == "__main__":
     sol = Solution()
     result = sol.climbStairs(10)
