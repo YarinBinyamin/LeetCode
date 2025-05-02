d666bc41e53201c8bfe3336f8e906471b3e56d5f
YarinBinyamin <yarin10binyamin@gmail.com>
2025-05-02 17:35:47 +0300

    s

--- src/Main2.java
+++ src/Main2.java
@@ -591,13 +591,7 @@ public class Main2 {
         return newHead;
 
     }
-    #Definition for singly-linked list.
-    #class ListNode:
-    #def __init__(self, val=0, next=None):
-    #self.val = val
-    #self.next = next
-    class Solution:
-        def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
 
 }
 
--- src/MainPython.py
+++ src/MainPython.py
@@ -232,6 +232,14 @@ class Solution:
             else:
                 left += 1 
         return False
+    
+    # Definition for singly-linked list.
+    # class ListNode:
+    #     def __init__(self, val=0, next=None):
+    #         self.val = val
+    #         self.next = next
+    class Solution:
+        def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
                 
             
                
